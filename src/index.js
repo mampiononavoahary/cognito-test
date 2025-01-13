e9839fe481af8bc1eb974cdf45517d25212a49ff
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-01-13 17:17:03 +0300

    feat: change redirect_uri

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import React from "react";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.eu-west-3.amazonaws.com/eu-west-3_Wnov89saO",
   client_id: "1hv99qg1nd0sk2g8eca9fmi6rr",
-  redirect_uri: "https://cognitostd22093",
+  redirect_uri: "https://cognitovoaharystd22093",
   response_type: "code",
   scope: "phone openid email",
 };
